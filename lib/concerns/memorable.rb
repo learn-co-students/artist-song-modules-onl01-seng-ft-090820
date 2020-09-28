5c45b6187e737529a538324a1af584834b4b98b0
J K <kj688895@gmail.com>
2020-09-28 00:48:43 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,21 @@
+module Memorable
+  module ClassInstances
+
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    self.all.count
+  end
+
+
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+
+
+
+end
