e803f67674ce5dfc8fc69ef82f09b4d6735ab8cd
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-09-29 22:19:48 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,24 +18,19 @@ class Artist
   def self.all
     @@artists
   end
-
   def self.reset_all
     self.all.clear
   end
-
   def self.count
     self.all.count
   end
-
   def add_song(song)
     @songs << song
     song.artist = self
   end
-
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
   def to_param
     name.downcase.gsub(' ', '-')
   end
